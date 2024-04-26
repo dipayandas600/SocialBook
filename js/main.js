9f46e5ba9687dbf01925b730ae426b0752b8a131
Dipayan Das <80384778+dipayandas600@users.noreply.github.com>
2024-04-26 01:56:06 -0700

    update html css and javascript files.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 // SICKY-NAVBER-START //
 
-window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 0) {
-        navbar.classList.add('sticky');
-    } else {
-        navbar.classList.remove('sticky');
-    }
-});
+// window.addEventListener('scroll', function() {
+//     const navbar = document.getElementById('navbar');
+//     if (window.scrollY > 0) {
+//         navbar.classList.add('sticky');
+//     } else {
+//         navbar.classList.remove('sticky');
+//     }
+// });
+
+// window.onscroll = function() {myFunction()};
+// var navber = document.getElementById("navber");
+// var sticky = navber.ooffsetTop;
+// function myFunction() {
+//     if (window.scrollY >= sticky) {
+//         navber.classList.add("sticky")
+//     } else {
+//         navber.classList.remove("sticky");
+//     }
+// }
 
 // SICKY-NAVBER-COMPLETE //
